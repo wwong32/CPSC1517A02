7636b3e5eae43baff34256997a61b2e3540309d0
wwong32 <johnathanwl_2@hotmail.com>
2019-10-21 22:44:50 -0600

    razorsolution

---
+++ RazorSolution/RazorSolution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RazorSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
